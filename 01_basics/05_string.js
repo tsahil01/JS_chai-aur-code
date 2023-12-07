64c3a9d8a343466373425db3b1b90783d35226bc
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-07 14:06:18 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,29 @@
+const name = "Sahil "
+const repoCount = 40
+
+// console.log((name + repoCount));
+// console.log(`Hello my name is ${name} and my repo count is${repoCount}`);
+
+const gameName = new String('sahiltiwaskargame') // to declare a string 
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); // to access prototype. Use this on browser console.  
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+
+const newName = gameName.substring(0, 5)
+// console.log(newName)
+
+
+const anotherName = gameName.slice(-1, 6)
+// console.log(anotherName)
+
+const anotherNameOne = "        Sahil       "
+// console.log(anotherNameOne)
+// console.log(anotherNameOne.trim())
+
+const url = "https://sahil.com/sahil%20tiwaskar"
+// console.log(url.replace('%20', '-'))
