f5d66213a58134016c0880e9222bbc4ff6b324ad
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-07 09:49:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_coverstion.js
@@ -0,0 +1,14 @@
+let score = "305s" // String
+console.log(typeof (score));
+
+let scoreInNumber = Number(score) // Number
+console.log(scoreInNumber)
+
+console.log(typeof(scoreInNumber));
+
+
+
+// "33" => 33
+// "33abc" => NaN (Not a number)
+// true => 1
+// false => 0
