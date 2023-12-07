ac9322db5bebc16cf2774886e1f96d626c1fabe3
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-07 17:21:19 +0000

    math and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,33 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(365) // declaring balance as Number type
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // round decimal
+
+
+const num1 = 23.47974
+// console.log(num1.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ********************** Maths *********************
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(3,1,6,8,4));
+// console.log(Math.max(3,1,6,8,4));
+
+// console.log(Math.round(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max-min + 1)) + min); // get number between min and max
