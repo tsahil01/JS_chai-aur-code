b33c99bb39aa960742b362e4b232af50aa852651
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-08 14:02:52 +0000

    array 2

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+const myArray = [0, 1, 2, 3, 5]
+const myHeros = ["Shatkiman", "ironman"]
+
+const myArray2 = new Array(1, 2, 3, 4)
+
+// console.log(myArray);
+
+// Array methods -->
+
+// myArray.push("Sahil");
+// console.log(myArray);
+// myArray.pop()
+// console.log(myArray);
+
+myArray.unshift(9) // adds element to the start of array
+myArray.shift() // to remove 9. Undo the above task
+// console.log(myArray);
+
+// myArray.sort()
+// console.log(myArray);
+
+const myArray3 = myArray.join() // result is in string not in array
+// console.log(myArray3);
+
+
+
+// Slice, Splice -->
+
+// console.log("A: ",myArray);
+
+// const myn1 = myArray.slice(1, 3)
+// console.log("B: ",myArray);
+// console.log(myn1);
+
+
+// const myn2 = myArray.splice(1, 3) // this will cut myArray elements in real
+// console.log("C: ",myArray);
+// console.log(myn2);
