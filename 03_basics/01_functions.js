94229b73df9630c929de7250012c95654b536256
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-09 13:32:51 +0000

    functions 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+    console.log("Hellooo");
+}
+
+
+// sayMyName()
+
+// const a  = sayMyName
+// a() // we can also do this!!!!!
+
+
+function addTwoNum(num1, num2){ // num1, num2 => parameters
+    return (num1 + num2)
+}
+
+console.log(addTwoNum(6, 5)); // 6,5 => arguments
+
