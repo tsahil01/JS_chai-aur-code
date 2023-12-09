24323d71e3fcab76cd88121c9dd020eeed5a1517
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-09 14:31:54 +0000

    scopes 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,27 @@ if(true){
 // console.log(a); // error
 // console.log(b); // error
 // console.log(c); // 30
+
+function one(){
+    const username = "Sahil"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); // inside function can access data of outside function!!
+    }
+    // console.log(website); // outside function cannot access data of inside function.
+
+    two()
+}
+
+// one()
+
+
+
+// ******************************* intresting **********************************
+
+const addTwoNum = function(num){
+    return num + 2
+}
+
+console.log(addTwoNum(5));
