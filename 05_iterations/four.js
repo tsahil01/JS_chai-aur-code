ca6ed3dd29d544c7620cbaf3da5eed0252c0fc22
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-11 18:33:53 +0000

    iterator 4

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,26 +1,16 @@
-const myObject = {
-    js: 'javascript',
-    cpp: 'C++',
-    rb: "ruby",
-    swift: "swift by apple"
+const myObj = {
+    js: "Javascript",
+    cpp: "C++",
+    py: "Python"
 }
 
-for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+for (const key in myObj) {
+    // console.log(`${key} : ${myObj[key]}`);
 }
 
-const programming = ["js", "rb", "py", "java", "cpp"]
+const prog = ["js", "rb", "cpp", "java"]
 
-for (const key in programming) {
-    //console.log(programming[key]);
+for (const i in prog) { // i gives the array keys(position)
+    // console.log(i);
+    console.log(prog[i]);
 }
-
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
-
-// for (const key in map) {
-//     console.log(key);
-// }
