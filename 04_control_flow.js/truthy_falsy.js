84249567a6c652ba4fa87844e242bcbbedd28c06
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-11 11:26:49 +0000

    truthy falsy

---
+++ 04_control_flow.js/truthy_falsy.js
@@ -0,0 +1,13 @@
+const userEmail = -4
+
+if(userEmail){
+    console.log("Got user email");
+} else{
+    console.log("Dont have user email");
+}
+
+// falsy values=>
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truty values=>
+// true, 1, 1>,  "0", 'false', " ", [], {}, function(){}, 
