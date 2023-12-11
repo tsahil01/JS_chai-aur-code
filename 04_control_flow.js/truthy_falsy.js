bf7bbdaa5261d695b5d82629e6354479ac6d428a
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-11 11:31:56 +0000

    Nullish Coalescing Operator(??)

--- 04_control_flow.js/truthy_falsy.js
+++ 04_control_flow.js/truthy_falsy.js
@@ -11,3 +11,16 @@ if(userEmail){
 
 // truty values=>
 // true, 1, 1>,  "0", 'false', " ", [], {}, function(){}, 
+
+//  Nullish Coalescing Operator(??): null undefined
+
+let val1;
+val1 = 5 ?? 10 
+val1 = null ?? 2 // checks safety... agar value nhi hai toh null assign kar do
+console.log(val1);
+
+val1 = undefined ?? 10
+console.log(val1);
+
+val1 = null ?? 10 ?? 20
+console.log(val1);
