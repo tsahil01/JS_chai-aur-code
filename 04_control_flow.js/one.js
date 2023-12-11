07641cc4953e57c49a348fd2247b1ca9095a8e9a
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-11 11:32:09 +0000

    Terniary Operator

--- 04_control_flow.js/one.js
+++ 04_control_flow.js/one.js
@@ -8,3 +8,8 @@
 
 // <, >, <=, >=, ==, ===, !=, !==
 
+// Terniary Operator -->
+// condition ? true : false
+
+const iceTea = 100
+iceTea > 80 ? console.log("greater than 80") : console.log("More than 80");
