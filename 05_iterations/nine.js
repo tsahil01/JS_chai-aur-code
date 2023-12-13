89e8f956072a308fa7c9efd69704161f1fedf2c6
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-13 15:22:12 +0000

    .reduce from MDN docs

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,10 +1,22 @@
-const myNums = [1, 2, 3]
+// const myNums = [1, 2, 3]
 
-// See .reduce() documentation on MDN
+// // See .reduce() documentation on MDN
 
-const myTotal = myNums.reduce( (acc, curval) => {
-    console.log(`acc: ${acc}, and currval: ${curval}`);
-    return acc + curval
-}, 0) // here 0 is acc initial value
+// const myTotal = myNums.reduce( (acc, curval) => {
+//     console.log(`acc: ${acc}, and currval: ${curval}`);
+//     return acc + curval
+// }, 0) // here 0 is acc initial value
 
-console.log(myTotal);
+// console.log(myTotal);
+
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce( 
+    (accumulator, currentValue) => accumulator + currentValue, initialValue,
+);
+
+// console.log(sumWithInitial);
+// Expected output: 10
