69cd5a0e0e4e1ae0e3c948fc8fb57f13a21e7611
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-13 15:22:21 +0000

    practice js

--- practice.js
+++ practice.js
@@ -1,12 +1,47 @@
-const arr = [1 , 4, 5, 7, 8, 7]
+// // Dates and Time
 
-// arr.forEach((item, index, ar) => {console.log(item, index, ar);})
+// const date = new Date()
+// // console.log(date);
+// console.log(date.getDay());
 
-const obj = {
-    name : "Sah",
-    class : 10
-}
+// const newDate = new Date(2023, 11, 23);
+// console.log(newDate);
 
-for (const key in obj) {
-    console.log(key);
-}
+
+// // // Arrays -->
+
+// const myarr = []
+
+// myarr.push( 20, 30, 50, 70)
+// myarr.unshift(60)
+// // console.log(myarr);
+
+// const myarray = [20, 50, 60, 25]
+
+// const arrthree = [...myarr, ...myarr]
+// // console.log(arrthree);
+
+
+// const user = {}
+// user.greet = (i) => console.log(i+i);
+
+// user.greet(2)
+
+
+// IIFE - immediate invoked function execution ---> ()();
+
+// (
+//     (item) => {
+//         console.log(item);
+// })(10);
+
+
+const mynums = [10, 50, 5, 2, 7, 3, 7]
+
+// const a = mynums.filter((i) => i>=10 )
+const a = mynums.map((i) => i*i)
+console.log(a);
+
+
+
+
