22c39629f45d25127caf0ae48d98aeec135f9c7d
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-26 15:45:48 +0530

    DOM practice

---
+++ DOM practice/script.js
@@ -0,0 +1,47 @@
+// const li = document.createElement('li')
+// li.setAttribute('class', 'item')
+// // li.innerHTML = "New Fruit"
+// // console.log(li)
+
+// const addText = document.createTextNode("New Fruit it is")
+// li.append(addText)
+// const parent = document.querySelector('.items-ul')
+// parent.append(li)
+
+// // console.log(parent.children[0].innerHTML);
+
+
+// const item = document.querySelector('.item')
+// // console.log(item.parentNode)
+// // console.log(item.parentElement)
+
+
+function addNewFruit(fruitName, className, parentClassName){
+    const li = document.createElement('li')
+    li.className = className
+
+    const text = document.createTextNode(`${fruitName}`)
+    li.append(text)
+
+    document.querySelector(`.${parentClassName}`).append(li)
+}
+
+function replaceFruit(fruitNumber, fruitName, className){
+    const li = document.createElement('li')
+    li.className = className
+
+    const text = document.createTextNode(`${fruitName}`)
+    li.append(text)
+
+    const repFru = document.querySelector(`li:nth-child(${fruitNumber})`)
+    repFru.replaceWith(li)
+}
+
+function deleteFruit(fruitNumber){
+    const delFru = document.querySelector(`li:nth-child(${fruitNumber})`)
+    delFru.remove()
+}
+
+addNewFruit("Kiwi", 'item', 'items-ul')
+replaceFruit(2, 'new apple', 'item')
+deleteFruit(4)
